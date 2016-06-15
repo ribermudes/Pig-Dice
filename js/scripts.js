11f38c8c570af94550cae3e5446b0260e7a3742a
Lauren Posey and Rachael Bermudes <lauren@>
2016-06-15 10:22:51 -0700

    working to create a random number generator

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// ===== Backend Logic =====
+// var turnScore = 0;
+// var player1 = 0;
+// var player2 = 0;
+
+
+
+function rollDie (min, max) {
+  return Math.floor(Math.random() * (min - max + 1)) + min;
+}
+
+var dieRoll;
+while(dieRoll = rollDie(1, 6)) === 1);
+
+// ===== Frontend Logic =====
+
+$(document).ready(function(){
+  $(" ").submit(function(event){
+    event.preventDefault();
